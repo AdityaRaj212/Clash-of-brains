b25563bf26642f0b9e0eafe81ccbf9bdd9ebdb96
AdityaRaj212 <aditya.education212@gmail.com>
2024-07-11 19:03:22 +0530

    removed type module from frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/AuthContext';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
+import { AuthProvider } from './context/AuthContext.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
